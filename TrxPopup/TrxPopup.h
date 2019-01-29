28f5ec8b6cbeabdb25d6eb6d00e93d2f3f08ffa9
Yaroslav <mail@gmail.com>
2019-01-29 05:12:26 -0500

    Initial Commit

---
+++ TrxPopup/TrxPopup.h
@@ -0,0 +1,19 @@
+//
+//  TrxPopup.h
+//  TrxPopup
+//
+//  Created by user on 29/01/2019.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrxPopup.
+FOUNDATION_EXPORT double TrxPopupVersionNumber;
+
+//! Project version string for TrxPopup.
+FOUNDATION_EXPORT const unsigned char TrxPopupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrxPopup/PublicHeader.h>
+
+
